27409f520c37129bb3d2341b801285fcb09923ed
Jan Biedermann <jan@kursator.de>
2019-06-03 14:28:37 +0200

    External Links must use A

--- isomorfeus/components/welcome_component.rb
+++ isomorfeus/components/welcome_component.rb
@@ -3,11 +3,11 @@ class WelcomeComponent < LucidComponent::Base
     H3 "Welcome to the Isomorfeus Framework"
     DIV "For the moment see:"
     BR()
-    Link(to: 'https://github.com/isomorfeus/') { 'The Isomorfeus Framework at Github' }
+    A(href: 'https://github.com/isomorfeus/') { 'The Isomorfeus Framework at Github' }
     BR()
     BR()
-    Link(to: 'https://join.slack.com/t/isomorfeus/shared_invite/enQtNjQzMDMzMTM2ODIzLWU1ZDBlZmE2MjMxMmY1YmY4MmZiYzY5MTU0NTc3OTNhMTgzY2M2YWQwZGNhZDczMjlkNGMwZWI2YmRhMjkyZjY') { "Join our Isomorfeus Slack space here" }
+    A(href: 'https://join.slack.com/t/isomorfeus/shared_invite/enQtNjQzMDMzMTM2ODIzLWU1ZDBlZmE2MjMxMmY1YmY4MmZiYzY5MTU0NTc3OTNhMTgzY2M2YWQwZGNhZDczMjlkNGMwZWI2YmRhMjkyZjY') { "Join our Isomorfeus Slack space" }
     SPAN " or "
-    Link(to: 'https://join.slack.com/t/isomorfeus/shared_invite/enQtNjQzMDMzMTM2ODIzLWU1ZDBlZmE2MjMxMmY1YmY4MmZiYzY5MTU0NTc3OTNhMTgzY2M2YWQwZGNhZDczMjlkNGMwZWI2YmRhMjkyZjY') { "our Gitter chat room here" }
+    A(href: 'https://gitter.im/isomorfeus/Lobby') { "our Gitter chat room" }
   end
 end
