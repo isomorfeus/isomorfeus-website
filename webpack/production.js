ad6b8adf3c12d560a149b6d6dd5f06a641f4d321
Jan Biedermann <jan@kursator.de>
2019-10-09 11:53:40 +0200

    Enable source maps for debug guide.

--- webpack/production.js
+++ webpack/production.js
@@ -94,7 +94,53 @@ const browser_debug_guide_config = {
             OPAL_DEVTOOLS_OBJECT_REGISTRY: true
         })
     ],
-    entry: { application_debug_guide: [path.resolve(__dirname, '../isomorfeus/imports/application_debug_guide.js')] }
+    entry: { application_debug_guide: [path.resolve(__dirname, '../isomorfeus/imports/application_debug_guide.js')] },
+    module: {
+        rules: [
+            {
+                test: /.scss$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: false, // set to false to speed up hot reloads
+                            minimize: true // set to false to speed up hot reloads
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            includePath: [path.resolve(__dirname, '../isomorfeus/styles')],
+                            sourceMap: false // set to false to speed up hot reloads
+                        }
+                    }
+                ]
+            },
+            {
+                // loader for .css files
+                test: /.css$/,
+                use: [ "style-loader", "css-loader" ]
+            },
+            {
+                test: /.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                use: [ "file-loader" ]
+            },
+            {
+                // opal-webpack-loader will compile and include ruby files in the pack
+                test: /.(rb|js.rb)$/,
+                use: [
+                    {
+                        loader: 'opal-webpack-loader',
+                        options: {
+                            sourceMap: true,
+                            hmr: false
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 };
 
 const ssr_config = {
