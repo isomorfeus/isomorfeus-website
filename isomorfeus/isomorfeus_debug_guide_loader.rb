135d6b9bcecf7fa92e03d45a975c7d840104f472
Jan Biedermann <jan@kursator.de>
2019-10-09 12:11:08 +0200

    Dont load things that are not needed.

--- isomorfeus/isomorfeus_debug_guide_loader.rb
+++ isomorfeus/isomorfeus_debug_guide_loader.rb
@@ -11,10 +11,10 @@ require 'isomorfeus-operation'
 
 Opal::Autoloader.add_load_path('debug_guide_components')
 
-require_tree 'policies'
-require_tree 'channels'
-require_tree 'data'
-require_tree 'operations'
+# require_tree 'policies'
+# require_tree 'channels'
+# require_tree 'data'
+# require_tree 'operations'
 require_tree 'debug_guide_components'
 
 Isomorfeus.start_app!
