c82f3236993bd7ad269adde71c8bb6851acf9827
Frédéric ZINGG <fredzingg@gmail.com>
2020-03-06 15:35:51 +0000

    overriding button styles test issues

--- app/components/header.rb
+++ app/components/header.rb
@@ -2,7 +2,7 @@ class Header < LucidMaterial::Component::Base
   styles do |theme|
     {
       appbar: {
-        backgroundColor: theme.palette.green.main
+        backgroundColor: theme.palette.info.main
       },
       grow: {
         flexGrow: 1
--- app/components/navigation_bar.rb
+++ app/components/navigation_bar.rb
@@ -1,6 +1,23 @@
 class NavigationBar < LucidMaterial::Component::Base
+
+  override_button_style = {
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+      label: {
+      textTransform: 'capitalize',
+    }
+  }
+
   styles do |theme|
     {
+      button_style: override_button_style,
       nav: {
         height: 70
       }
@@ -10,7 +27,7 @@ class NavigationBar < LucidMaterial::Component::Base
   render do
     Mui.Box(display: `{ xs: 'none', lg: 'block' }`) do
       NAV(class: styles.nav) do
-        Mui.Button(color: 'secondary', variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
+        Mui.Button(class_name: styles.button_style, color: 'secondary', variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://twitter.com/isomorfeus') {MuiIcons.Twitter}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://github.com/isomorfeus') {MuiIcons.GitHub}
 
