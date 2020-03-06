ca227198f3b57fe7df5d1151e37017ce821fb96f
Jan Biedermann <jan@kursator.de>
2020-03-06 17:24:05 +0100

    Fix styling.

--- app/components/navigation_bar.rb
+++ app/components/navigation_bar.rb
@@ -1,23 +1,19 @@
 class NavigationBar < LucidMaterial::Component::Base
 
-  override_button_style = {
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 3,
-      border: 0,
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    },
-    label: {
-      textTransform: 'capitalize',
-    }
-  }
-
   styles do |theme|
     {
-      button_style: override_button_style,
+      button_style_root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+      button_style_label: {
+          textTransform: 'capitalize',
+      },
       nav: {
         height: 70
       }
@@ -27,7 +23,7 @@ class NavigationBar < LucidMaterial::Component::Base
   render do
     Mui.Box(display: `{ xs: 'none', lg: 'block' }`) do
       NAV(class_name: styles.nav) do
-        Mui.Button(class_name: styles.button_style, color: 'secondary', variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
+        Mui.Button(classes: `{ root: #{styles.button_style_root}, label: #{styles.button_style_label} }`, color: 'secondary', variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://twitter.com/isomorfeus') {MuiIcons.Twitter}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://github.com/isomorfeus') {MuiIcons.GitHub}
 
