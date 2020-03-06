f8a615b48413d17286bd1a6c13369cce7e3d44be
Frédéric ZINGG <fredzingg@gmail.com>
2020-03-06 16:29:27 +0000

    Auto stash before merge of "new_site" and "origin/new_site"

--- app/components/navigation_bar.rb
+++ app/components/navigation_bar.rb
@@ -1,18 +1,33 @@
 class NavigationBar < LucidMaterial::Component::Base
 
+  override_button_style = {
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
+    },
+    label: {
+      textTransform: 'capitalize'
+    }
+  }
+
   styles do |theme|
     {
-      button_style_root: {
+      root: {
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         borderRadius: 3,
         border: 0,
         color: 'white',
         height: 48,
         padding: '0 30px',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)'
       },
-      button_style_label: {
-          textTransform: 'capitalize',
+        label: {
+        textTransform: 'capitalize'
       },
       nav: {
         height: 70
@@ -23,7 +38,7 @@ class NavigationBar < LucidMaterial::Component::Base
   render do
     Mui.Box(display: `{ xs: 'none', lg: 'block' }`) do
       NAV(class_name: styles.nav) do
-        Mui.Button(classes: `{ root: #{styles.button_style_root}, label: #{styles.button_style_label} }`, color: 'secondary', variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
+        Mui.Button(class_name: "#{styles.root} #{styles.label}", variant: 'contained', component: `Opal.global.Link`, to: '/docs') {"Documentation"}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://twitter.com/isomorfeus') {MuiIcons.Twitter}
         Mui.Button(color: 'secondary', variant: 'contained', component: 'span', href: 'https://github.com/isomorfeus') {MuiIcons.GitHub}
 
